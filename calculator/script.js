fd07b219e80e44acdaaf7657dbbb36869bd3810a
c0by-server <c0by-server@deb-server.deb-hp>
2024-08-21 12:31:22 +0000

    commit: adding keyboard functionality

--- calculator/script.js
+++ calculator/script.js
@@ -1,23 +1,18 @@
 const input = document.getElementById("input-text");
 const buttons = document.querySelectorAll("button");
+
 let calculationDone = false;
 
 function calculation(expression) {
-  //   console.log(expression);
   try {
     return new Function("return " + expression)();
   } catch (error) {
-    return `Error`;
+    return `Malformed Operation`;
   }
 }
 
 function operation(buttonValue) {
-  if (
-    calculationDone &&
-    buttonValue !== "=" &&
-    buttonValue !== "C" &&
-    buttonValue !== "Del"
-  ) {
+  if (calculationDone && buttonValue !== "=" && buttonValue !== "C" && buttonValue !== "Del") {
     input.value = "";
     calculationDone = false;
   }
@@ -33,9 +28,24 @@ function operation(buttonValue) {
     input.value += buttonValue;
   }
 }
+
 buttons.forEach((button) => {
   let buttonValue = button.innerText;
   button.addEventListener("click", function () {
     operation(buttonValue);
   });
+});
+
+document.addEventListener("keydown", (event) => {
+  const key = event.key;
+
+  if (!isNaN(key) || "+-*/.".includes(key)) {
+    operation(key);
+  } else if (key === "Enter" || key === '=') {
+    operation("=");
+  } else if (key === "Backspace") {
+    operation("Del");
+  } else if (key.toUpperCase() === "C") {
+    operation("C");
+  }
 });
